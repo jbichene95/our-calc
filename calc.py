db46565b2656d7c1b4d18a3cc94df7915094e46d
Opeyemi <opeadebisi01@gmail.com>
2023-02-09 12:49:43 +0100

    calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,18 @@
+
+
+res = input("subtraction or adition(s or r) ?")
+
+
+
 value1 = int(input("get value1 "))
 
 value2  =int( input("value 2"))
+if res =="a":
+ result = value1+ value2
 
-result = value1+ value2
-
-print(result)
+ print(result)
+elif res=="s":
+ result= value1 - value2
+ print(result)
+else:
+ print("unreconized")
